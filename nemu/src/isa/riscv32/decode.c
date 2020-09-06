cb1137aa44b1ae4bd6fe1223200822a26e26332d
tracer-ics2019 <tracer@njuics.org>
2020-09-06 23:53:06 +0800

    >  compile 171860578 zzy Linux zzy-virtual-machine 5.4.0-45-generic #49~18.04.2-Ubuntu SMP Wed Aug 26 16:29:02 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  23:53:06 up  6:50,  1 user,  load average: 0.15, 0.13, 0.10 94e53fff5916a924897079c060f72d847beaef3f

--- nemu/src/isa/riscv32/decode.c
+++ nemu/src/isa/riscv32/decode.c
@@ -56,7 +56,7 @@ make_DHelper(J) {
   int32_t simm = (decinfo.isa.instr.simm20 << 20) | (decinfo.isa.instr.imm10_1 << 1) | (decinfo.isa.instr.imm11_ << 11) | (decinfo.isa.instr.imm19_12 << 12);
   decode_op_i(id_src, simm, true);
 
-  printf("%d\n",simm);
+  printf("sb%d\n",simm);
 
   print_Dop(id_src->str, OP_STR_SIZE, "0x%x", simm);
 
