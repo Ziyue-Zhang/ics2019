60a4c1cb38de192758fa4df118bd0e379016d760
tracer-ics2019 <tracer@njuics.org>
2020-09-07 00:43:18 +0800

    >  compile 171860578 zzy Linux zzy-virtual-machine 5.4.0-45-generic #49~18.04.2-Ubuntu SMP Wed Aug 26 16:29:02 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  00:43:18 up  7:40,  1 user,  load average: 0.21, 0.23, 0.10 68d5aee0d65b43de733a45a97ba2584134d700

--- nemu/src/isa/riscv32/exec/control.c
+++ nemu/src/isa/riscv32/exec/control.c
@@ -2,12 +2,9 @@
 
 make_EHelper(jmp) {
   // the target address is calculated at the decode stage
-  /*rtl_add(&t0, cpu.pc, 4);
-  rtl_sr(id_dest->reg, &t0, 4);
-
-  rtl_add(&t0, int(cpu.pc), &id_src->val);
-
-  rtl_j(t0);*/
+  
+  rtl_sr(id_dest->reg, &id_src2->val, 4);
+  rtl_j(decinfo.jmp_pc);
 
   print_asm_template2(jal);
 }
