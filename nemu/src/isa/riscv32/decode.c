223d4d476f670d9d89f47caf562c83104a7f0d5a
tracer-ics2019 <tracer@njuics.org>
2020-09-07 02:01:26 +0800

    >  compile 171860578 zzy Linux zzy-virtual-machine 5.4.0-45-generic #49~18.04.2-Ubuntu SMP Wed Aug 26 16:29:02 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  02:01:26 up  8:58,  1 user,  load average: 0.50, 0.41, 0.29 32821f62e638cf41683b044fafd5d00e119b22c7

--- nemu/src/isa/riscv32/decode.c
+++ nemu/src/isa/riscv32/decode.c
@@ -30,7 +30,7 @@ make_DHelper(U) {
 }
 
 make_DHelper(ld) {
-  printf("aa%d",decinfo.isa.instr.rs1);
+  printf("aa%d\n",decinfo.isa.instr.rs1);
   decode_op_r(id_src, decinfo.isa.instr.rs1, true);
   decode_op_i(id_src2, decinfo.isa.instr.simm11_0, true);
 
