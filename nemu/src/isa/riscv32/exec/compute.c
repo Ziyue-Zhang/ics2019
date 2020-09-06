51ca5ef7d29c8be11ed4cf566706527791ee3053
tracer-ics2019 <tracer@njuics.org>
2020-09-07 00:49:15 +0800

    >  compile 171860578 zzy Linux zzy-virtual-machine 5.4.0-45-generic #49~18.04.2-Ubuntu SMP Wed Aug 26 16:29:02 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  00:49:15 up  7:46,  1 user,  load average: 0.10, 0.13, 0.09 9626a2c6573c0ec490e7f13a6124960cdd842b63

--- nemu/src/isa/riscv32/exec/compute.c
+++ nemu/src/isa/riscv32/exec/compute.c
@@ -9,7 +9,7 @@ make_EHelper(lui) {
 make_EHelper(add) {
 
   rtl_add(&id_dest->val, &id_src->val, &id_src2->val);
-  printf("sb%d\n",id_dest->val);
+  printf("sb%08x\n",id_dest->val);
   rtl_sr(id_dest->reg, &id_dest->val, 4);
 
   print_asm_template2(add);
