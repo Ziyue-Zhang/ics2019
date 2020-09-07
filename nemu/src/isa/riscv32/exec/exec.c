72c1c7358894656097d70e1d67e4a723a08dd721
tracer-ics2019 <tracer@njuics.org>
2020-09-07 13:02:08 +0800

    >  compile 171860578 zzy Linux zzy-virtual-machine 5.4.0-45-generic #49~18.04.2-Ubuntu SMP Wed Aug 26 16:29:02 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  13:02:08 up 11:20,  1 user,  load average: 0.62, 0.45, 0.22 c9183fda205e6b9acfdc2e4970014b3e1b541479

--- nemu/src/isa/riscv32/exec/exec.c
+++ nemu/src/isa/riscv32/exec/exec.c
@@ -22,8 +22,8 @@ static make_EHelper(store) {
 static OpcodeEntry opcode_table [32] = {
   /* b00 */ IDEX(ld, load), EMPTY, EMPTY, EMPTY, IDEX(I, add), IDEX(U, auipc), EMPTY, EMPTY,
   /* b01 */ IDEX(st, store), EMPTY, EMPTY, EMPTY, IDEX(R, add), IDEX(U, lui), EMPTY, EMPTY,
-  /* b10 */ EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, IDEX(B, beq),
-  /* b11 */ EMPTY, IDEX(I, jalr), EX(nemu_trap), IDEX(J,jmp), EMPTY, EMPTY, EMPTY, EMPTY,
+  /* b10 */ EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY,
+  /* b11 */ IDEX(B, beq), IDEX(I, jalr), EX(nemu_trap), IDEX(J,jmp), EMPTY, EMPTY, EMPTY, EMPTY,
 };
 
 void isa_exec(vaddr_t *pc) {
