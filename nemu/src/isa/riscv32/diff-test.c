30fef90b9c0d44f45cf2809b239c3304f00f0b3e
tracer-ics2019 <tracer@njuics.org>
2020-09-08 11:33:44 +0800

    >  compile 171860578 zzy Linux zzy-virtual-machine 5.4.0-45-generic #49~18.04.2-Ubuntu SMP Wed Aug 26 16:29:02 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:33:44 up  2:28,  1 user,  load average: 0.93, 0.77, 0.71 eca2a2cf3abdf0885a2da55eda445ebf550f6478

--- nemu/src/isa/riscv32/diff-test.c
+++ nemu/src/isa/riscv32/diff-test.c
@@ -5,13 +5,14 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   if(cpu.pc != pc){
       printf("%08x %08x\n",cpu.pc, pc);
   }
+  bool flag=true;
   for(int i = 0; i < 32; i++){
       if(reg_l(i) != ref_r->gpr[i]._32){
           printf("Error in %s, qemu:0x%08x, nemu:0x%08x\n", reg_name(i, 32), ref_r->gpr[i]._32, reg_l(i));
-          return false;
+          flag=false;
       }
   }
-  return true;
+  return flag;
 }
 
 void isa_difftest_attach(void) {
